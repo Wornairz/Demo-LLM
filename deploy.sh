48300be9c62096cd9d11c90a837d00671b4c4385
Alessandro Catalano <alessandrocatalano999@gmail.com>
2025-06-27 23:12:09 +0200

    fix kube

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,11 @@
 # --- Configurazione ---
 # Tag per le immagini
 LLM_SERVER_IMG="demo-llm-server:latest"
-SPRING_LLM_IMG="demo-llm:latest"
+SPRING_LLM_IMG="demo-spring-llm:latest"
 
 # Percorsi relativi ai Dockerfile
 LLM_SERVER_DIR="./llm-server"
-SPRING_LLM_DIR="./llm-spring"
+SPRING_LLM_DIR="./spring-llm"
 
 # Manifest Kubernetes
 K8S_MANIFEST="./k8s-demo-llm.yaml"
@@ -20,6 +20,11 @@ docker build -t "${LLM_SERVER_IMG}" "${LLM_SERVER_DIR}"
 echo "Building Spring LLM image (${SPRING_LLM_IMG})..."
 docker build -t "${SPRING_LLM_IMG}" "${SPRING_LLM_DIR}"
 
+#k3d cluster create demo-llm-cluster --port "8080:80@loadbalancer" --port "6443:6443@loadbalancer"
+#k3d image import -c demo-llm-cluster "${LLM_SERVER_IMG}" "${SPRING_LLM_IMG}"
+
 # --- Deploy su Kubernetes ---
 echo "Applying Kubernetes manifest (${K8S_MANIFEST})..."
+kubectl create namespace "${K8S_NAMESPACE}"
 kubectl apply -f "${K8S_MANIFEST}"
+kubectl -n demo-llm port-forward svc/spring-llm 8080:80
